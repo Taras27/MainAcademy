fa162913c5b90edb7495a48809c99038df4e2445
Taras Horyn (HP635) <horyn.ua@gmail.com>
2020-01-26 20:33:54 +0200

    Airport panel project is completed

--- AirPort_Panell_Module/AirPortPanell/Program.cs
+++ AirPort_Panell_Module/AirPortPanell/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Globalization;
 
 namespace AirPortPanell
 {
@@ -19,8 +20,8 @@ namespace AirPortPanell
         }
         struct flyingInfo
         {
-            public string timeArrival; //
-            public string timeDeparture; //
+            public DateTime timeArrival; //
+            public DateTime timeDeparture; //
             public string flightNumber;//
             public string cityOfArrival;//
             public string cityOfDeparture;//
@@ -41,7 +42,8 @@ namespace AirPortPanell
                     "\r\n\tAdd arrival or departure: 1" +
                     "\r\n\tShow main table: 2" +
                     "\r\n\tEdit arrival or departure: 3" +
-                    "\r\n\tSearch fligt: 4");
+                    "\r\n\tSearch fligt: 4" +
+                    "\r\n\tSearch fligt hour: 5");
                 int userChoise = int.Parse(Console.ReadLine());
                 switch(userChoise)
                 {
@@ -57,7 +59,12 @@ namespace AirPortPanell
                         Console.Clear();
                         showMainHat();
                         showTableHat();
-                        showFly();
+
+                        for(int i=0; i<myFlying.Length; i++)
+                        {
+                            showFly(i);
+                        }
+
                         Console.WriteLine("Press any key to continue!");
                         Console.ReadKey();
                         break;
@@ -74,8 +81,35 @@ namespace AirPortPanell
 
                     case 4: //search fly
                         Console.Clear();
-                        showMainHat();
-                        showTableHat();
+                        Console.WriteLine("Choise pametr for search:" +
+                            "\r\n\tSearch by flight number: 1" +
+                            "\r\n\tSearch by time of arrival: 2" +
+                            "\r\n\tSearch by arrival city: 3" +
+                            "\r\n\tSearch by departure city: 4");
+                        int tmp = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Enter your key for search:");
+                        string keySearch = Console.ReadLine();
+
+                        searchFly(keySearch, tmp);
+
+                        Console.WriteLine("Press any key to continue!");
+                        Console.ReadKey();
+                        break;
+
+                    case 5://one hour
+                        Console.Clear();
+                        Console.WriteLine("Search of the flight which is the nearest!");
+                        Console.WriteLine("Enter time value in format: dd.mm.yyyy hh:mm:ss :");
+                        DateTime tmpDate = DateTime.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter port:");
+                        string port = Console.ReadLine();
+                        Console.WriteLine("Enter digit to choise port:" +
+                            "\r\n\t In arrival: 1" +
+                            "\r\n\t In departure: 2");
+                        int choiseParam = int.Parse(Console.ReadLine());
+
+                        sortFly(tmpDate, port, choiseParam);
 
                         Console.WriteLine("Press any key to continue!");
                         Console.ReadKey();
@@ -86,11 +120,11 @@ namespace AirPortPanell
 
         public static void addFly(int numFly)
         {
-            Console.WriteLine("Enter time departure:");
-            myFlying[numFly].timeDeparture = Console.ReadLine();
+            Console.WriteLine("Enter time departure in format dd.mm.yyyy hh:mm:ss:");
+            myFlying[numFly].timeDeparture = DateTime.Parse (Console.ReadLine());
 
-            Console.WriteLine("Enter time arrival:");
-            myFlying[numFly].timeArrival = Console.ReadLine();
+            Console.WriteLine("Enter time arrival in format dd.mm.yyyy hh:mm:ss:");
+            myFlying[numFly].timeArrival = DateTime.Parse(Console.ReadLine());
 
             Console.WriteLine("Enter flight number:");
             myFlying[numFly].flightNumber = Console.ReadLine();
@@ -136,25 +170,24 @@ namespace AirPortPanell
             myFlying[numFly].gate = Console.ReadLine();
         }
 
-        public static void showFly()
+        public static void showFly(int num)
         {            
             string strTmp = null;
-            for(int tmp = 0; tmp<myFlying.Length; tmp++)
-            {
-                if (myFlying[tmp].timeArrival != null)
+            
+                if (myFlying[num].flightNumber != null)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
                     string[] arrSum = {
-                            (tmp+1).ToString().PadLeft(3),
-                            myFlying[tmp].timeArrival.PadLeft(20),
-                            myFlying[tmp].timeDeparture.PadLeft(20),
-                            myFlying[tmp].flightNumber.PadLeft(20),
-                            myFlying[tmp].cityOfArrival.PadLeft(20),
-                            myFlying[tmp].cityOfDeparture.ToString().PadLeft(20),
-                            myFlying[tmp].airLines.PadLeft(20),
-                            myFlying[tmp].terminal.PadLeft(20),
-                            myFlying[tmp].flyingStatus.ToString().PadLeft(20),
-                            myFlying[tmp].gate.PadLeft(20),
+                            (num+1).ToString().PadLeft(3),
+                            myFlying[num].timeArrival.ToString().PadLeft(20),
+                            myFlying[num].timeDeparture.ToString().PadLeft(20),
+                            myFlying[num].flightNumber.PadLeft(20),
+                            myFlying[num].cityOfArrival.PadLeft(20),
+                            myFlying[num].cityOfDeparture.ToString().PadLeft(20),
+                            myFlying[num].airLines.PadLeft(20),
+                            myFlying[num].terminal.PadLeft(20),
+                            myFlying[num].flyingStatus.ToString().PadLeft(20),
+                            myFlying[num].gate.PadLeft(20),
                             };
                     Console.WriteLine(string.Join("|", arrSum));
                     Console.ResetColor();
@@ -163,7 +196,7 @@ namespace AirPortPanell
                 else
                 {
                     strTmp = "---";
-                    string[] arrSum = { (tmp+1).ToString().PadLeft(3),
+                    string[] arrSum = { (num+1).ToString().PadLeft(3),
                             strTmp.PadLeft(20), strTmp.PadLeft(20),
                             strTmp.PadLeft(20), strTmp.PadLeft(20),
                             strTmp.PadLeft(20), strTmp.PadLeft(20),
@@ -173,7 +206,7 @@ namespace AirPortPanell
                     showTableBoard();
                     Console.ResetColor();
                 }
-            }           
+                       
         }
 
         public static void showMainHat()
@@ -212,16 +245,15 @@ namespace AirPortPanell
             Console.WriteLine(delim);
             Console.ResetColor();
         }
-    
         public static void editFly (int numberEdit)
         {
-            if(myFlying[numberEdit].timeDeparture != null)
+            if(myFlying[numberEdit].flightNumber != null)
             {
-                Console.WriteLine("Enter time departure:");
-                myFlying[numberEdit].timeDeparture = Console.ReadLine();
+                Console.WriteLine("Enter time departure in format dd.mm.yyyy hh:mm:ss:");
+                myFlying[numberEdit].timeDeparture = DateTime.Parse (Console.ReadLine());
 
-                Console.WriteLine("Enter time arrival:");
-                myFlying[numberEdit].timeArrival = Console.ReadLine();
+                Console.WriteLine("Enter time arrival in format dd.mm.yyyy hh:mm:ss:");
+                myFlying[numberEdit].timeArrival = DateTime.Parse(Console.ReadLine());
 
                 Console.WriteLine("Enter flight number:");
                 myFlying[numberEdit].flightNumber = Console.ReadLine();
@@ -275,6 +307,155 @@ namespace AirPortPanell
                 Thread.Sleep(5000);
             }
 
+        }
+    
+        public static void searchFly(string key, int param )
+        {
+            showMainHat();
+            showTableHat();
+            switch (param)
+            {
+                case 1: //flight number
+                    for(int i=0;i<myFlying.Length; i++)
+                    {
+                        if(myFlying[i].flightNumber == key)
+                        {
+                            showFly(i);                           
+                        }
+                    }
+                    break;
+
+                case 2: //time of arrival
+                    for (int i = 0; i < myFlying.Length; i++)
+                    {
+                        if (myFlying[i].timeArrival.ToString() == key)
+                        {
+                            showFly(i);                            
+                        }
+                    }
+                    break;
+
+                case 3: //arrival port
+                    for (int i = 0; i < myFlying.Length; i++)
+                    {
+                        if (myFlying[i].cityOfArrival == key)
+                        {
+                            showFly(i);                            
+                        }
+                    }
+                    break;
+
+                case 4: // departure port
+                    for (int i = 0; i < myFlying.Length; i++)
+                    {
+                        if (myFlying[i].cityOfDeparture == key)
+                        {
+                            showFly(i);                            
+                        }
+                    }
+                    break;
+            }
+        }
+
+        public static void sortFly(DateTime keyValueTime, string port, int choise)
+        {
+            Console.Clear();
+            showMainHat();
+            showTableHat();
+
+            flyingInfo[] myFlyingTmp = new flyingInfo[20];
+            flyingInfo[] myFlyingOne = new flyingInfo[1];
+
+            int counter = 0;
+            for (int i = 0; i < myFlyingTmp.Length; i++)
+            {
+                TimeSpan diferenceTime = TimeSpan.Zero;
+                if (choise == 1)
+                {
+                    diferenceTime = myFlying[i].timeArrival - keyValueTime;
+                }
+                if (choise == 2)
+                {
+                    diferenceTime = myFlying[i].timeDeparture - keyValueTime;
+                }
+                if (diferenceTime.Minutes <= 59 && diferenceTime.Hours < 1
+                    && diferenceTime.Minutes >= 0 && diferenceTime.Hours >= 0)
+                {
+                    myFlyingTmp[counter] = myFlying[i];
+                    counter++;
+                }
+            }
+
+            flyingInfo[] myFlyingTwo = new flyingInfo[20];
+            if (choise == 1)  //to city arrival
+            {
+                for (int i = 0; i < myFlyingTmp.Length; i++)
+                {
+                    if (myFlyingTmp[i].cityOfArrival == port)
+                    {
+                        myFlyingTwo[i] = myFlyingTmp[i];
+                    }
+                }
+            }
+            if (choise == 2) // from city departure
+            {
+                for (int i = 0; i < myFlyingTmp.Length; i++)
+                {
+                    if (myFlyingTmp[i].cityOfDeparture == port)
+                    {
+                        myFlyingTwo[i] = myFlyingTmp[i];
+                    }
+                }
+            }           
+
+            for (int i = 0; i < myFlyingTwo.Length - 1; i++)
+            {
+                for (int j = i + 1; j < myFlyingTwo.Length; j++)
+                {
+                    if (choise == 1)
+                    {
+                        if (myFlyingTwo[i].timeArrival > myFlyingTwo[j].timeArrival)
+                        {
+                            myFlyingOne[0] = myFlyingTwo[i];
+                            myFlyingTwo[i] = myFlyingTwo[j];
+                            myFlyingTwo[j] = myFlyingOne[0];
+                        }
+                    }
+                    if (choise == 2)
+                    {
+                        if (myFlyingTwo[i].timeDeparture > myFlyingTwo[j].timeDeparture)
+                        {
+                            myFlyingOne[0] = myFlyingTwo[i];
+                            myFlyingTwo[i] = myFlyingTwo[j];
+                            myFlyingTwo[j] = myFlyingOne[0];
+                        }
+                    }
+                }
+            }
+            int count = 0;
+            for(int i = 0; i< myFlyingTwo.Length; i++)
+            {
+                if (myFlyingTwo[i].flightNumber != null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    string[] arrSum = {
+                            (++count).ToString().PadLeft(3),
+                            myFlyingTwo[i].timeArrival.ToString().PadLeft(20),
+                            myFlyingTwo[i].timeDeparture.ToString().PadLeft(20),
+                            myFlyingTwo[i].flightNumber.PadLeft(20),
+                            myFlyingTwo[i].cityOfArrival.PadLeft(20),
+                            myFlyingTwo[i].cityOfDeparture.ToString().PadLeft(20),
+                            myFlyingTwo[i].airLines.PadLeft(20),
+                            myFlyingTwo[i].terminal.PadLeft(20),
+                            myFlyingTwo[i].flyingStatus.ToString().PadLeft(20),
+                            myFlyingTwo[i].gate.PadLeft(20),
+                            };
+                    Console.WriteLine(string.Join("|", arrSum));
+                    Console.ResetColor();
+                    showTableBoard();
+                }
+            }
+            
         }
     }
 }
